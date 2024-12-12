c18404d5fb6e1b4159cc39a0f224b02925aae06e
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-12 10:09:51 +0100

    snack 01

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,19 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+const firstLetter = (words) => {
+    const letters = [];
+    for(let i = 0; i < words.length; i++) {
+        letters.push(words[i][0])
+    }
+    return letters;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(firstLetter(names));
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
