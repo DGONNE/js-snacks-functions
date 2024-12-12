702c0cb28be7afdbcf162848a56ccd0c975759de
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-12 10:32:30 +0100

    snack 04

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,13 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+const hello = (name) => {
+    return `Ciao ${name}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const result = hello(userName);
+console.log(result)
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
